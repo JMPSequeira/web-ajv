652ee7f2429b263565a6faae639c6f147afa78cd
JMPSequeira <email.sequeira@gmail.com>
2021-05-30 21:58:50 +0100

    remove exports from src/index

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,2 @@
 export { validatorFactory } from "./gen-lib/validator-factory";
 export { e, anyfy } from "./gen-lib/utils";
-
-export { Options } from "./steps/types";
-
-export {
-    WebAjvAsyncValidateFn,
-    WebAjvValidateFn,
-    WebAjvError,
-    WebAjvResult,
-    AsyncAjvValidationFn,
-    AjvValidationFn,
-} from "./gen-lib/types";
